e071b30f7faf3ba04098e9afc898701b78849300
hisamuddin siddique <sabbusiddique40@gmail.com>
2024-10-22 19:49:26 +0530

    added bin changes

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c721339446423d474ef2e71f40cf4303974a1d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45dc8b6ec18c5b8e35fba02e0306ea70d72878e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
